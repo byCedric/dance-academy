02c55f26cb387099535b12992d5b97de3e920399
Remus Richard Dumitrache <Remus.Richard@autentia.com>
2019-09-07 13:51:10 +0200

    unused import

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
-import ClassDetailScreen from "../screens/ClassDetailScreen";
 
 export default createAppContainer(
     createSwitchNavigator({
